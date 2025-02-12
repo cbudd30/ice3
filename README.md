e335e463c4ae5544cb56004d75864d4f6670b005
cbudd30 <c27caitlyn.budd@afacademy.af.edu>
2025-02-11 21:30:25 -0700

    completed ice

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ Then it **elaborates** the entity defined by `$TB_ENTITY`
 Finally, the workflow **runs** the simulation. If successful then it will quietly exit with a `0` code.
 If any of the `assert` statements fail then GHDL will cease the simulation and exit with non-zero code; this will also cause the workflow to fail.
 Assert statements of other severity levels will be reported, but not fail the workflow.
+
+![ripple carry waveform](ripplewaveform.png)
+
+![sketch of basy entity](topbasyentity.jpg)
+
+## Documentation
+Recieved help from Capt Yarbrough on which ports are used for top_basy vs. ripple carry and the importance of the level of architecture. All other work is my own.
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -45,11 +45,6 @@ library ieee;
 
 entity top_basys3 is
 	port(
-	       A : in STD_LOGIC_VECTOR (3 downto 0);
-           B : in STD_LOGIC_VECTOR (3 downto 0);
-           Cin : in STD_LOGIC;
-           S : out STD_LOGIC_VECTOR (3 downto 0);
-           Cout : out STD_LOGIC;
 	
 		-- Switches
 		sw		:	in  std_logic_vector(15 downto 0);
@@ -62,14 +57,13 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
     -- declare the component of your top-level design
-    component full_adder  is
-    Port ( A : in STD_LOGIC;
-           B : in STD_LOGIC; 
+    component ripple_adder is
+        port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
            Cin : in STD_LOGIC;
-           S : out STD_LOGIC;
-           Cout : out STD_LOGIC
-       );
-   end component full_adder;
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           Cout : out STD_LOGIC);
+      end component ripple_adder;
 
     -- declare any signals you will need
       signal w_carry  : STD_LOGIC_VECTOR(3 downto 0);
@@ -77,41 +71,24 @@ architecture top_basys3_arch of top_basys3 is
   
 begin
 	-- PORT MAPS --------------------
-    full_adder_0: full_adder
+   ripple_adder1: ripple_adder
     port map(
-    A => sw(1),
-    B => sw(12),
-    Cin => sw(0), 
-    S => led(0), 
-    Cout => w_carry(0)
-    );
-
-    full_adder_1: full_adder
-    port map(
-        A => sw(2),
-        B => sw(13),
-        Cin => w_carry(0), 
-        S => led(1), 
-        Cout => w_carry(1)
-    );
-    
-   full_adder_2: full_adder
-    port map(
-        A    => sw(3),
-        B    => sw(14),
-        Cin   => w_carry(1),   -- Directly to input here
-        S     => led(2),
-        Cout  => w_carry(2)
-    );
-    
-    full_adder_3: full_adder
-    port map(
-        A    => sw(4),
-        B     => sw(15),
-        Cin   => w_carry(2),   -- Directly to input here
-        S     => led(3),
-        Cout  => led(15)
-    );
+        Cin => sw(0),
+        A(0) => sw(1),
+        A(1) => sw(2),
+        A(2) => sw(3),
+        A(3) => sw(4),
+        B(0) => sw(12),
+        B(1) => sw(13),
+        B(2) => sw(14),
+        B(3) => sw(15),
+        S(0) => led(0),
+        S(1) => led(1),
+        S(2) => led(2),
+        S(3) => led(3),
+        Cout => led(15)
+        
+     );
 	---------------------------------
 	
 	-- CONCURRENT STATEMENTS --------
